9919d74d8f3fc4b0cc30ea14decd204713866720
Jonathan Foster <fostertheweb@gmail.com>
2022-11-19 15:12:06 -0500

    col by default

--- src/screens/Chores.tsx
+++ src/screens/Chores.tsx
@@ -37,7 +37,7 @@ export default function ChoresScreen() {
       onPress={() => alert("Edit " + item.name)}
       onLongPress={() => alert("Complete " + item.name)}
       style={{ margin: 8 }}
-      className="flex-1 border border-gray-300 rounded shadow bg-white p-4 flex flex-col items-center justify-center"
+      className="flex-1 border border-gray-300 rounded shadow bg-white p-4 flex items-center justify-center"
     >
       <View className="w-16 h-16 rounded-full bg-gray-200"></View>
       <Text className="text-black mt-2">{item.name}</Text>
