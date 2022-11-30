00f6b57bc4570b80ac66562052789aeeb211e713
Jonathan Foster <fostertheweb@gmail.com>
2022-11-30 16:04:17 -0500

    picker is weird

--- src/screens/AddChore.tsx
+++ src/screens/AddChore.tsx
@@ -13,11 +13,13 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import uuid from "react-native-uuid";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { StatusBar } from "expo-status-bar";
+import { Controller, useForm } from "react-hook-form";
+import { Picker } from "@react-native-picker/picker";
 
 import IconSearch from "./IconSearch";
 import ChoreCard from "../components/ChoreCard";
 import { Chore } from "../types/Chores";
-import { Controller, useForm } from "react-hook-form";
+import IconsIcon from "../components/IconsIcon";
 
 export default function AddChoreScreen({ navigation }) {
   const {
@@ -64,31 +66,37 @@ export default function AddChoreScreen({ navigation }) {
     setChore((chore) => ({ icon: icon.images["64"], ...chore }));
     setIconSearchOpen(false);
   }
+  const [selectedLanguage, setSelectedLanguage] = useState();
 
   return (
     <View className="p-8 flex items-center">
       <Pressable onPress={() => setIconSearchOpen(true)}>
-        <View className="flex items-center justify-center w-28 h-28 bg-blue-100 border border-blue-400 rounded-full">
+        <View className="flex items-center justify-center w-28 h-28 bg-white border-2 border-gray-200 rounded-full">
           {icon ? (
             <Image
               style={{ height: 64, width: 64 }}
               source={{ uri: icon.images["64"] }}
             />
           ) : (
-            <Text>+</Text>
+            <View className="h-28 w-28 flex items-center justify-center">
+              <IconsIcon />
+              <Text className="mt-2 font-semibold text-gray-400 text-xs">
+                Choose Icon
+              </Text>
+            </View>
           )}
         </View>
       </Pressable>
 
-      <View>
-        <Text>What are you doing?</Text>
+      <View className="w-full">
+        <Text className="text-xl mt-6">What are you doing?</Text>
         <Controller
           name="description"
           control={control}
           rules={{ required: true }}
           render={({ field: { onBlur, onChange, value } }) => (
             <TextInput
-              className="mt-4 w-full p-2 text-center text-3xl border-b-2 border-gray-200"
+              className="w-full py-2 text-xl border-b-2 border-gray-200"
               placeholder="Chore Description"
               onChangeText={(text) => onChange(text)}
               onBlur={onBlur}
@@ -97,23 +105,42 @@ export default function AddChoreScreen({ navigation }) {
           )}
         />
 
-        <Text>How often?</Text>
-        <Controller
-          name="interval"
-          control={control}
-          rules={{ required: true }}
-          render={({ field: { onBlur, onChange, value } }) => (
-            <TextInput
-              className="mt-4 w-full p-2 text-center text-3xl border-b-2 border-gray-200"
-              placeholder="Number"
-              onChangeText={(text) => onChange(text)}
-              onBlur={onBlur}
-              value={value}
-            />
-          )}
-        />
-
-        <Text>{JSON.stringify(errors)}</Text>
+        <Text className="text-xl mt-6">How often?</Text>
+        <View className="flex flex-row items-center">
+          <Text className="mr-2 text-xl">Every</Text>
+          <Controller
+            name="interval"
+            control={control}
+            rules={{ required: true }}
+            render={({ field: { onBlur, onChange, value } }) => (
+              <TextInput
+                className="py-2 text-xl mr-2 border-b-2 border-gray-200"
+                placeholder="Number"
+                onChangeText={(text) => onChange(text)}
+                onBlur={onBlur}
+                value={value}
+              />
+            )}
+          />
+          <Controller
+            name="interval_unit"
+            control={control}
+            rules={{ required: true }}
+            render={({ field: { onBlur, onChange, value } }) => (
+              <Picker
+                selectedValue={value}
+                onValueChange={onChange}
+                onBlur={onBlur}
+                style={{ height: 48, width: 150 }}
+                itemStyle={{ height: 48, fontSize: 18 }}
+              >
+                <Picker.Item label="days" value="day" />
+                <Picker.Item label="weeks" value="week" />
+                <Picker.Item label="months" value="month" />
+              </Picker>
+            )}
+          />
+        </View>
       </View>
 
       <Modal
